679311ec11668197e36c5968bdff67856a17405e
Makar-Makarych <makar.ee@ya.ru>
2021-06-13 18:58:36 +0300

    test

--- pars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-variable=`efibootmgr  | awk '/BootOrder: / {print $2}'`
-if [[ $variable ]]; then
-    echo "Мы проверили Ваше железо и рекомендуем Вам выбрать 'УСТАНОВКА В EFI'."
-else
-    echo "Согласно Вашему железу мы Вам рекомендуем выбрать 'УСТАНОВКА В MBR'!"
-fi
