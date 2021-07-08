92a97701b93e70045fdb57007e718e186bfdc81a
Makar-Makarych <makar.ee@ya.ru>
2021-07-08 16:38:46 +0300

    Изм.

--- t-efi-btr3.sh
+++ t-efi-btr3.sh
@@ -383,7 +383,7 @@ pacman -S exfat-utils ntfs-3g gvfs --noconfirm
 #----------------   ПО
 clear
 pacman -S file-roller gparted p7zip unace lrzip gvfs-afc htop xterm gvfs-mtp neofetch blueman flameshot firefox firefox-i18n-ru  --noconfirm 
-pacman -S archlinux-appstream-data
+
 #------------------------ Дополнительное ПО
 
 $DIALOG --title " ДОПОЛНИТЕЛНОЕ ПО" --clear \
