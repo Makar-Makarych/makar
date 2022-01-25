d9e3b8aab4c7888ed1fb7b0029321e5f7b2dd3ca
mak <83972728+Makar-Makarych@users.noreply.github.com>
2022-01-25 23:59:16 +0300

    Update starch.sh

--- starch.sh
+++ starch.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 
-OPTION=$(whiptail --title  " LANGUAGE " --menu  "
-
-  " 15 60 4 \
-"1" "Russian" \
-"2" "English" 3>&1 1>&2 2>&3)
+#OPTION=$(whiptail --title  " LANGUAGE " --menu  "
+#
+#   " 15 60 4 \
+# "1" "Russian" \
+# "2" "English" 3>&1 1>&2 2>&3)
  
-exitstatus=$?
-if [ $exitstatus = 0 ];  
-    then
-        clear
-        echo "" 
-    else
-        clear
-        echo ""
-fi
+# exitstatus=$?
+# if [ $exitstatus = 0 ];  
+#     then
+#         clear
+#         echo "" 
+#     else
+#         clear
+#         echo ""
+# fi
 
-#-----------  Переход по выбору  ------------------------
+# #-----------  Переход по выбору  ------------------------
 
-case $OPTION in
-                "1")
+# case $OPTION in
+#                 "1")
                 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Makar-Makarych/makar/main/t-efi-btr1.sh)"
-             break
-             ;;
-                "2")
-                sh -c "$(curl -fsSL https://raw.githubusercontent.com/Makar-Makarych/makar/main/t-efi-btr1-en.sh)"
+#              break
+#              ;;
+#                 "2")
+#                 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Makar-Makarych/makar/main/t-efi-btr1-en.sh)"
              
-             break
-             ;;
-                255)
-     	        echo " ESC.";;
-esac
+#              break
+#              ;;
+#                 255)
+#      	        echo " ESC.";;
+# esac
