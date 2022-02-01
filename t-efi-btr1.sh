778c52054839d7f8404102572452a00143d0a722
Makar-Makarych <makar.ee@yandex.ru>
2022-02-01 14:01:10 +0300

    ок

--- t-efi-btr1.sh
+++ t-efi-btr1.sh
@@ -33,7 +33,7 @@ OPTION=$(whiptail --title  " ТИП УСТАНОВКИ " --menu  "
 
   Выберите вариант, как Вы хотите установить систему" 15 60 4 \
 "1" "UEFI + BtrFS + Subvolumes" \
-"2" "UEFI + Ext4 (пока не работает)" \
+"2" "UEFI + Ext4" \
 "3" "MBR (Legacy) + BtrFS + Subvolumes" \
 "4" "MBR (Legacy) + Ext4"  3>&1 1>&2 2>&3)
  
