b799465a52702962a29c7ab51e25ddad581377fa
mak <83972728+Makar-Makarych@users.noreply.github.com>
2023-09-28 13:02:09 +0300

    Update mbr-ext.sh

--- mbr-ext.sh
+++ mbr-ext.sh
@@ -196,6 +196,9 @@ fi
 #echo "VerbosePkgLists" >> /etc/pacman.conf
 #echo "IloveCandy" >> /etc/pacman.conf
 #echo "ParallelDownloads = 5" >> /etc/pacman.conf
+sed -i 's/#Color/Color/' /etc/pacman.conf
+sed -i 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 
 #-------------------- Установка базы  
 
